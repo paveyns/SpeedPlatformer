c41908db3b39f384f3d4c75b36d27e1e6815b431
Adam Hesselbrock <hesselam@mail.uc.edu>
2019-12-09 13:27:43 -0500

    Added Platforms

--- game.js
+++ game.js
@@ -25,6 +25,62 @@ platform1 = {
   y:450,
 };
 
+platform2 = {
+  height:5,
+  width:350,
+  x:0,
+  y:375,
+};
+
+platform3 = {
+  height:5,
+  width:500,
+  x:350,
+  y:300,
+};
+
+platform4 = {
+  height:5,
+  width:200,
+  x:0,
+  y:300,
+};
+
+platform5 = {
+  height:5,
+  width:200,
+  x:200,
+  y:225,
+};
+
+platform6 = {
+  height:5,
+  width:200,
+  x:450,
+  y:175,
+};
+
+platform7 = {
+  height:5,
+  width:350,
+  x:0,
+  y:125,
+};
+
+platform8 = {
+  height:5,
+  width:500,
+  x:360,
+  y:70,
+};
+
+platform9 = {
+  height:5,
+  width:350,
+  x:0,
+  y:30,
+};
+
 controller = {
 
   left:false,
@@ -112,6 +168,46 @@ loop = function() {
   context.fillStyle = "#00ff00";
   context.beginPath();
   context.rect(platform1.x, platform1.y, platform1.width, platform1.height);
+  context.fill();
+
+  context.fillStyle = "#00ff00";
+  context.beginPath();
+  context.rect(platform2.x, platform2.y, platform2.width, platform2.height);
+  context.fill();
+
+  context.fillStyle = "#00ff00";
+  context.beginPath();
+  context.rect(platform3.x, platform3.y, platform3.width, platform3.height);
+  context.fill();
+
+  context.fillStyle = "#00ff00";
+  context.beginPath();
+  context.rect(platform4.x, platform4.y, platform4.width, platform4.height);
+  context.fill();
+
+  context.fillStyle = "#00ff00";
+  context.beginPath();
+  context.rect(platform5.x, platform5.y, platform5.width, platform5.height);
+  context.fill();
+
+  context.fillStyle = "#00ff00";
+  context.beginPath();
+  context.rect(platform6.x, platform6.y, platform6.width, platform6.height);
+  context.fill();
+
+  context.fillStyle = "#00ff00";
+  context.beginPath();
+  context.rect(platform7.x, platform7.y, platform7.width, platform7.height);
+  context.fill();
+
+  context.fillStyle = "#00ff00";
+  context.beginPath();
+  context.rect(platform8.x, platform8.y, platform8.width, platform8.height);
+  context.fill();
+
+  context.fillStyle = "#00ff00";
+  context.beginPath();
+  context.rect(platform9.x, platform9.y, platform9.width, platform9.height);
   context.fill();
 
   //main ground
